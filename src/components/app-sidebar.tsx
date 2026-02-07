76da306eff8a235170b3f89e0d5a8bde95801dd6
MurakawaTakuya <murakawatakuya5@gmail.com>
2026-02-07 17:51:49 +0900

    ログアウトボタンの表示位置を変更

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -209,7 +209,7 @@ export function AppSidebar() {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent
                   className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-                  side={isMobile ? "bottom" : "right"}
+                  side={isMobile ? "bottom" : "top"}
                   align="end"
                   sideOffset={4}
                 >
