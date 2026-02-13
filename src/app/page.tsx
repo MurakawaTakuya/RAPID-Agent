d4593efe0fabb26e6f49bef163e57bc2797c29b2
MurakawaTakuya <murakawatakuya5@gmail.com>
2026-02-13 23:55:43 +0900

    headerを画面上に固定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
   return (
     <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 mb-8">
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 mb-8 sticky top-0 z-10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <SidebarTrigger className="-ml-1" />
         <AnimatedThemeToggler className="ml-auto" />
       </header>
