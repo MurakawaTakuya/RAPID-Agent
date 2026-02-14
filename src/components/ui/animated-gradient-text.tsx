55e51bc2340a62ee8c0cfde6918640a03766bd7e
MurakawaTakuya <murakawatakuya5@gmail.com>
2026-02-15 03:46:18 +0900

    gradient textのグラデーションカラーを調整

--- src/components/ui/animated-gradient-text.tsx
+++ src/components/ui/animated-gradient-text.tsx
@@ -6,14 +6,18 @@ export interface AnimatedGradientTextProps extends ComponentPropsWithoutRef<"div
   speed?: number;
   colorFrom?: string;
   colorTo?: string;
+  darkColorFrom?: string;
+  darkColorTo?: string;
 }
 
 export function AnimatedGradientText({
   children,
   className,
   speed = 1,
-  colorFrom = "#54dbf3ff",
-  colorTo = "#ae5cf1ff",
+  colorFrom = "#3ba5f6ff",
+  colorTo = "#f45cf6ff",
+  darkColorFrom = "#54e3f3ff",
+  darkColorTo = "#c75cf1ff",
   ...props
 }: AnimatedGradientTextProps) {
   return (
@@ -23,10 +27,13 @@ export function AnimatedGradientText({
           "--bg-size": `${speed * 300}%`,
           "--color-from": colorFrom,
           "--color-to": colorTo,
+          "--color-from-dark": darkColorFrom,
+          "--color-to-dark": darkColorTo,
         } as React.CSSProperties
       }
       className={cn(
         `animate-gradient inline bg-gradient-to-r from-[var(--color-from)] via-[var(--color-to)] to-[var(--color-from)] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+        `dark:from-[var(--color-from-dark)] dark:via-[var(--color-to-dark)] dark:to-[var(--color-from-dark)]`,
         className
       )}
       {...props}
