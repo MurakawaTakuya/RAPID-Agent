63e057f80b4c916c37881b7c184dfed171ce0c50
MurakawaTakuya <murakawatakuya5@gmail.com>
2026-02-15 15:32:30 +0900

    Revert "npx heroui-cli@latest add toast"
    This reverts commit 35fe4f9e74eaf37b56457220f81595f195126371.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-const { heroui } = require("@heroui/theme");
 module.exports = {
   darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(toast|spinner).js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [heroui()],
+  plugins: [],
 };
