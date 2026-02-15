35fe4f9e74eaf37b56457220f81595f195126371
MurakawaTakuya <murakawatakuya5@gmail.com>
2026-02-15 15:30:48 +0900

    npx heroui-cli@latest add toast

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+const { heroui } = require("@heroui/theme");
 module.exports = {
   darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/components/(toast|spinner).js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [heroui()],
 };
